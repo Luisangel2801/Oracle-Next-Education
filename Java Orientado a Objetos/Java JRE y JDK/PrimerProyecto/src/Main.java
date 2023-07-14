bea885d87d9cdb5570a8aa0004be1c49b966d386
Luis Ángel Cruz Díaz <luisangelcruzdiaz@icloud.com>
2023-07-14 01:18:09 -0600

    Apregamos ejemplos para el manejo de caracteres y cadenas de caracteres

--- Java Orientado a Objetos/Java JRE y JDK/PrimerProyecto/src/Main.java
+++ Java Orientado a Objetos/Java JRE y JDK/PrimerProyecto/src/Main.java
@@ -1,16 +1,27 @@
 public class Main {
     public static void main(String[] args) {
-        int edad = 23; // variable de tipo entero
+        int edad = 23;                              // variable de tipo entero
         System.out.println("edad: " + edad);
         edad = 22 + 45;
         System.out.println("edad: " + edad);
-        double salario = 1250.50; // variable de tipo flotante
+        double salario = 1250.50;                   // variable de tipo flotante
         System.out.println("Salario: " + salario);
 
+        // Conversiones
         double variableDecimal = 10.84;
-        int variableEntero = (int)variableDecimal; // Cast
+        int variableEntero = (int)variableDecimal;  // Cast
         System.out.println("moldeo: " + variableEntero);
 
+        // Manejo de caracteres
+        char caracter = '@';
+        System.out.println(caracter);
+        caracter = 65;
+        System.out.println(caracter);
+        caracter = (char)(caracter + 1);            // Cast
+        System.out.println(caracter);
 
+        // Manejo de cadenas de caracteres
+        String palabra = "Alura Cursos";
+        System.out.println(palabra + " ONE");
     }
 }
