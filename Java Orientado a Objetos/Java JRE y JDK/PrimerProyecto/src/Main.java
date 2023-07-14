82260d97682f531e0032c5e694bb513674e59a9a
Luis Ángel Cruz Díaz <luisangelcruzdiaz@icloud.com>
2023-07-14 00:03:56 -0600

    Creamos el repositorio para el programa ONE, en específico para la especialización Back-end

---
+++ Java Orientado a Objetos/Java JRE y JDK/PrimerProyecto/src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+        int edad = 23; // variable de tipo entero
+        System.out.println("edad: " + edad);
+        edad = 22 + 45;
+        System.out.println("edad: " + edad);
+        double salario = 1250.50; // variable de tipo flotante
+        System.out.println("Salario: " + salario);
+
+        double variableDecimal = 10.84;
+        int variableEntero = (int)variableDecimal; // Cast
+        System.out.println("moldeo: " + variableEntero);
+
+
+    }
+}
