58a9260274a0dd3a7aac63eb424d457e9e22a9a7
Luis Ángel Cruz Díaz <luisangelcruzdiaz@icloud.com>
2023-07-23 16:03:58 -0600

    Se crear el proyecto para practicar la estructura for

---
+++ Java Orientado a Objetos/Java JRE y JDK/Ciclos/src/Ciclos_For.java
@@ -0,0 +1,11 @@
+public class Ciclos_For {
+    public static void main(String[] args) {
+        // Uso de la estructura for
+        int total = 0;
+        // Mientras que contador sea menor que 10
+        for (int contador = 0; contador < 10; contador++) {
+            total += contador;
+        }
+        System.out.println("El total es: " + total);
+    }
+}
